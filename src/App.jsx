9857ea35cd7f443ed848076ad1a3cefa6da2d438
SojitraMit <sojitramit57757@gmail.com>
2026-03-01 17:25:54 +0530

    signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { SignInButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserAvatar,
+  UserButton,
+  UserProfile,
+} from "@clerk/clerk-react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Welcome to Talent IQ</h1>
-      <SignInButton mode="model" />
+      <SignedOut>
+        <SignInButton mode="model" />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton mode="model">Sign Out</SignOutButton>
+        <UserAvatar></UserAvatar>
+      </SignedIn>
+      <UserButton />
     </>
   );
 }
